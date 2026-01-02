a7fb913c2cf46bfdad5555aa7aa71a8e645b19e5
alanv33 <alanvu2005@gmail.com>
2026-01-01 18:52:02 -0800

    Fixed expo router entry bug

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="home" options={{headerShown: true, title:"Your Flashcard Sets"}} />
+      <Stack.Screen name="index" options={{headerShown: true, title:"Your Flashcard Sets"}} />
       <Stack.Screen name="study/[deckId]" options={{ headerShown: true}} />
     </Stack>
   );
